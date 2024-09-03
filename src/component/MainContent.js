e57672bbdc7de4cf9dc8ed81953f1588694330d2
nouryy17 <dacinour16@gmail.com>
2024-09-03 20:43:08 +0200

    add style for mainContent part

--- src/component/MainContent.js
+++ src/component/MainContent.js
@@ -1,17 +1,18 @@
 import img from "../images/illustration-intro.svg"
+import "../style/MainContent.sass"
 export default function () {
     return(
         <section className="main-content">
-        <div className="container">
+        
           <div className="text-content">
             <h1>Bring everyone together to build better products.</h1>
             <p>Manage makes it simple for software teams to plan day-to-day tasks while keeping the larger team goals in view.</p>
-            <a href="#" className="btn-get-started">Get Started</a>
+            <span className="">Get Started</span>
           </div>
           <div className="stats-content">
             <img src={img}></img>
           </div>
-        </div>
+        
       </section>
     )
 }
