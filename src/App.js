e92f704b144cf9ab7d6d7870242f01c5148ecfe9
nouryy17 <dacinour16@gmail.com>
2024-09-03 20:09:47 +0200

    add mainContent part

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import "./style/style.scss"
 import Header from "./component/header";
+import MainContent from "./component/MainContent"
 function App() {
   return (
    <div>
     <Header></Header>
+    <MainContent></MainContent>
    </div>
   );
 }
