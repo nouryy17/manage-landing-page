52ccf074c90904137ee688f2848401c1cdff8b8a
nouryy17 <dacinour16@gmail.com>
2024-09-05 11:43:11 +0200

    add style for  teamwork part

--- src/component/TeamWork.js
+++ src/component/TeamWork.js
@@ -1,6 +1,7 @@
+import "../style/TeamWork.sass"
 export default function () {
     return (
-        <div>
+        <div className="TeamWork">
             <h1>Simplify how your team works today</h1>
             <span>Get Started </span>
 
