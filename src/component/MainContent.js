18584af16a9530145eaca24c4d61460ca723bf06
nouryy17 <dacinour16@gmail.com>
2024-09-06 15:30:40 +0200

    Add some animations to a section MainContent

--- src/component/MainContent.js
+++ src/component/MainContent.js
@@ -1,16 +1,17 @@
 import img from "../images/illustration-intro.svg"
 import "../style/MainContent.sass"
+import 'animate.css'
 export default function () {
     return(
         <section className="main-content">
         
           <div className="text-content">
-            <h1>Bring everyone together to build better products.</h1>
-            <p>Manage makes it simple for software teams to plan day-to-day tasks while keeping the larger team goals in view.</p>
-            <span className="">Get Started</span>
+            <h1 className="animate__animated  animate__backInDown">Bring everyone together to build better products.</h1>
+            <p className="animate__animated animate__bounceInLeft">Manage makes it simple for software teams to plan day-to-day tasks while keeping the larger team goals in view.</p>
+            <span  className="animate__animated animate__bounceInLeft">Get Started</span>
           </div>
           <div className="stats-content">
-            <img src={img}></img>
+            <img className="animate__animated animate__rollIn" src={img}></img>
           </div>
         
       </section>
